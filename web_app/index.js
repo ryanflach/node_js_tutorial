e2ad9913b2aec9b65eb41f9af9dcc4ff5ac5dd1f
Ryan Flach <ryanflach@users.noreply.github.com>
2016-11-29 12:41:43 -0800

    Add BodyParser and CookieParser

--- web_app/index.js
+++ web_app/index.js
@@ -2,10 +2,18 @@ const fs = require('fs');
 const options = JSON.parse(fs.readFileSync('package.json'));
 const http = require('http');
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
-
 const server = http.createServer(app);
+
+app.use(cookieParser);
+
+app.use((req, res, next) => {
+  console.log(req.cookies);
+  res.cookie('demo', 'test');
+  next();
+});
 
 app.use('/demo/:id', (req, res) => {
   res.send(`params: ${JSON.stringify(req.params)}<br>query: ${JSON.stringify(req.query)}`);

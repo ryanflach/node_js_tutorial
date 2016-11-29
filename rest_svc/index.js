86301e31d8c085c49d5942ba77ff08f6e4332e88
Ryan Flach <ryanflach@users.noreply.github.com>
2016-11-29 10:46:58 -0800

    Add BodyParser

--- rest_svc/index.js
+++ rest_svc/index.js
@@ -1,10 +1,12 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const widgetRouter = require('./routers/widgets');
 
 const app = express();
 const server = http.createServer(app);
 
+app.use('/api', bodyParser.json());
 app.use('/api', widgetRouter);
 
 server.listen(3000, () => {

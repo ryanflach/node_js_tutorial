f59942dc305c9acca3d7539a0e7198c3572b0bfe
Ryan Flach <ryanflach@users.noreply.github.com>
2016-11-29 12:56:01 -0800

    Add express-session and passport for local auth - Note: Does not work due to missing example database file

--- web_app/index.js
+++ web_app/index.js
@@ -4,20 +4,62 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const { Strategy: LocalStrategy } = require('passport-local');
 const app = express();
 const server = http.createServer(app);
+const db = require('./database');
 
-app.use(cookieParser);
+passport.use(new LocalStrategy({
+  usernameField: 'username',
+  passwordField: 'password'
+}, (username, password, done) => {
+  if (db.authenticate(username, password)) {
+    done(null, {
+      username: username
+    });
+  } else {
+    done(null, false);
+  }
+}));
 
-app.use((req, res, next) => {
-  console.log(req.cookies);
-  res.cookie('demo', 'test');
-  next();
+const sessions = new Map();
+
+passport.serializeUser((user, cb) => {
+  sessions.set(user.username, user);
+  cb(null, user.username);
 });
 
-app.use('/demo/:id', (req, res) => {
-  res.send(`params: ${JSON.stringify(req.params)}<br>query: ${JSON.stringify(req.query)}`);
-})
+passport.deserializeUser((username, cb) => {
+  cb(null, sessions.get(username));
+});
+
+app.use(cookieParser);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'secret'
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'www', 'login.html'));
+});
+
+app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login'}));
+
+app.use((req, res, next) => {
+  if (!req.user) {
+    res.redirect('/login');
+    return;
+  }
+
+  next();
+});
 
 app.use(express.static(options.webServer.folder));
 
